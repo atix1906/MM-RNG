a7fc8265ff7d5d4cb0c5ccd2a17ee2aad82c4e5b
Thorsten Sommer <service@sommer-engineering.com>
2018-01-27 20:40:46 +0100

    Use Python instead of bash client

--- rng.sh
+++ rng.sh
@@ -3,5 +3,6 @@ while true
 do
     randomNumber=$(($RANDOM % 1000 + 1))
 	echo "$randomNumber"
+    echo -e "import redis\n r = redis.StrictRedis(host=$REDIS_HOST, port=$REDIS_PORT, db=0)\n r.rpush(\"rng::list\", $randomNumber)" | python
 	sleep 1
 done
