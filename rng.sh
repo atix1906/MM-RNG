98df5ad8a25765750e743935dd03962df90d735e
Thorsten Sommer <service@sommer-engineering.com>
2018-01-27 20:58:58 +0100

    Fixed syntax

--- rng.sh
+++ rng.sh
@@ -3,6 +3,6 @@ while true
 do
     randomNumber=$(($RANDOM % 1000 + 1))
 	echo "$randomNumber"
-    echo -e "import redis\n r = redis.StrictRedis(host=$REDIS_HOST, port=$REDIS_PORT, db=0)\n r.rpush(\"rng::list\", $randomNumber)" | python
+    echo -e "import redis\nr = redis.StrictRedis(host=\"$REDIS_HOST\", port=$REDIS_PORT, db=0)\nr.rpush(\"rng::list\", $randomNumber)" | python3
 	sleep 1
 done
